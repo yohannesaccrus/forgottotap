7a1eaced7dc9fd515c2acc6db226f42ca27eb9c0
Yohannes Alldy <yohannes.alldy@seamoney.com>
2024-07-01 15:21:40 +0700

    fix: add new page

---
+++ app/report/data/1.tsx
@@ -0,0 +1,131 @@
+
+export enum statusType {
+  InProgress,
+  Done,
+  Issue
+}
+
+export const data = {
+  main: [
+    {
+      number: 211444,
+      numberEpic: 205488,
+      title: '[ID-FE][CS][Admin] Change the display of payment method name in CS admin',
+      pm: 'tianpeng.lizhou',
+      assigne: 'condro.wiyono',
+      scope: ['AS', 'CS'],
+      role: ['reviewer'],
+      status: 1,
+      releasePlan: '07.v1'
+    },
+    {
+      number: 203102,
+      numberEpic: 198499,
+      title: '[ID-FE][CS][TH][Admin] Enhance Revise Action of Tax Invoice Page',
+      pm: 'marcus.eegx',
+      assigne: 'condro.wiyono',
+      scope: ['CS'],
+      role: ['reviewer'],
+      status: 1,
+      releasePlan: '07.v1'
+    },
+    {
+      number: 207133,
+      numberEpic: 198416,
+      title: '[ID-FE][FE][Admin] Add Payment Option Columns and Filter of Direct Debit',
+      pm: 'tianpeng.lizhou',
+      assigne: 'adi.almajid',
+      scope: ['AS'],
+      role: ['reviewer'],
+      status: 1,
+      releasePlan: '07.v1'
+    },
+    {
+      number: 207135,
+      numberEpic: 198416,
+      title: '[ID-FE][FE][Admin] Add Direct Debit as Payment Option in AS Payment Settings',
+      pm: 'tianpeng.lizhou',
+      assigne: 'adi.almajid',
+      scope: ['AS'],
+      role: ['reviewer'],
+      status: 1,
+      releasePlan: '07.v1'
+    },
+    {
+      number: 214297,
+      numberEpic: 213572,
+      title: '[ID-FE][CS][Admin] Change CS withdrawal Hyperlink',
+      pm: 'alicia.thamsx',
+      assigne: 'condro.wiyono',
+      scope: ['CS'],
+      role: ['reviewer'],
+      status: 1,
+      releasePlan: '07.v2'
+    },
+    {
+      number: 207143,
+      numberEpic: 200125,
+      title: '[ID-FE][CS][Admin] Update CS pages to AS Admin Wallet Request Changes',
+      pm: 'marcus.eegx',
+      assigne: 'condro.wiyono',
+      scope: ['CS'],
+      role: ['reviewer'],
+      status: 1,
+      releasePlan: '07.v2'
+    },
+    {
+      number: 215959,
+      numberEpic: 215957,
+      title: '[FE][CS] Refactor of Tax Invoice Page',
+      pm: '',
+      assigne: 'yohannes.alldy',
+      scope: ['CS'],
+      role: ['developer'],
+      status: 1,
+      releasePlan: 'TBA in Q3'
+    }
+  ],
+  platform: [
+    {
+      number: 214784,
+      numberEpic: 198304,
+      title: '[ID-FE] Improve UI in Layout and Products - Checkout with ShopeePay page',
+      pm: 'alicia.thamsx',
+      assigne: 'yohannes.alldy',
+      scope: ['Website'],
+      role: ['developer'],
+      status: 1,
+      releasePlan: '07.v1'
+    },
+    {
+      number: 215794,
+      numberEpic: 198304,
+      title: '[ID-FE] Glitch when clicking the sidebar',
+      pm: 'alicia.thamsx',
+      assigne: 'yohannes.alldy',
+      scope: ['Website'],
+      role: ['developer'],
+      status: 1,
+      releasePlan: '07.v1'
+    },
+    {
+      number: 214480,
+      numberEpic: '',
+      title: 'Modify grpc-client to support multiple endpoint',
+      pm: '',
+      assigne: 'yohannes.alldy',
+      scope: ['Platform'],
+      role: ['developer'],
+      status: 0,
+      releasePlan: 'TBA Adhoc'
+    }
+  ],
+  others: [
+    {
+      title: 'Shopeepay FE Academic Session - Git',
+      assigne: 'yohannes.alldy',
+      role: ['speaker'],
+      status: 0,
+    },
+  ]
+}
