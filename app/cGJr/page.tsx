040f5a87a57d28cece569e2bd969c1128c285d5d
Yohannes Alldy <yohannes.alldy@seamoney.com>
2024-07-08 18:21:45 +0700

    fix: add new desc

--- app/cGJr/page.tsx
+++ app/cGJr/page.tsx
@@ -29,6 +29,20 @@ const Hadi = () => {
             </h1>
           </section>
           <section className="section-content member">
+            <p className={raleway.className}>
+              Potion and Poison for us most of the times, nothing I could say except thank you for all these moments too with Julian until this current time, orange days, dinners in SCBD, shopee festival, all the goood and bad. When I said
+              poison and potion, I really mean it, Poison means you can laugh with us together or even triggered it for "someone", and potion means you were also good friend since I enter shopee Mas Bangun.
+              Just don't put Edwin in or it would be Poison Poison Potion, hahaha, just a joke.
+            </p>
+            <p className={raleway.className}>
+              Things were going hard ya Mas Bangun, I also see what you've been faced on during this current time and recently,
+              "More zoom to grow" just not making everyone grows.
+              I hope slowly your team and your stallions will rise again, end this suffering. Thank you too for being a good and humorous figure to all FE.
+            </p>
+            <p className={raleway.className}>
+              I enjoy all of our lunch moment together with Julian, eventhough I missed the first peda pelangi in SCBD, hope to keep this on but I'll skip if its release week. I'm sorry if the one
+              I gave you under the magnum hood is too small compared to the others, hope it can be seen.
+            </p>
             <h3 className={raleway.className} style={{ textAlign: "center" }}>
               We're Friends, no Worries
             </h3>
