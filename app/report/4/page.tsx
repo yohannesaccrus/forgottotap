956f02273514f0c4c672740e2732a2376e2f664e
Yohannes Alldy <yohannes.alldy@seamoney.com>
2024-08-12 13:05:27 +0700

    fix: add report 4

---
+++ app/report/4/page.tsx
@@ -0,0 +1,141 @@
+import { Raleway } from "next/font/google";
+import dayjs from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
+import Link from "next/link";
+
+import { data, statusType } from "../data/4";
+
+import '../custom.scss';
+
+const raleway = Raleway({
+  subsets: ["cyrillic"],
+  display: "swap",
+  weight: ["300", "400", "700"],
+});
+
+dayjs.extend(isBetween);
+
+const Report = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="content report">
+          <section className="section-title">
+            <h2 className={raleway.className}>
+              Bi-Weekly Report #4
+            </h2>
+            <p>Period: <span>{dayjs().subtract(2, 'week').format('dddd, D MMMM YYYY')} - {dayjs().format('dddd, D MMMM YYYY')}</span></p>
+          </section>
+          <section className="section-content works">
+            <h3>Main Area</h3>
+            <ul>
+              {data.main.map((work) => (
+                <li>
+                  <div className="work">
+                    <div className="links">
+                      <Link href={`https://jira.shopee.io/browse/SPPAY-${work.number}`}>JIRA Task</Link>
+                      <Link href={`https://jira.shopee.io/browse/SPPAY-${work.numberEpic}`}>JIRA's Epic</Link>
+                    </div>
+                    <p className="title">{work.title}</p>
+                    <div className="info">
+                      <p>scope:</p>
+                      <p className="value">{work.scope.join(', ')}</p>
+                    </div>
+                    <div className="info">
+                      <p>assigne:</p>
+                      <p className="value">{work.assigne}</p>
+                    </div>
+                    <div className="info">
+                      <p>pm:</p>
+                      <p className="value">{work.pm || 'none'}</p>
+                    </div>
+                    <div className="info">
+                      <p>release plan:</p>
+                      <p className="value">{work.releasePlan || 'TBA'}</p>
+                    </div>
+                    <div className="info">
+                      <p>role(s)</p>
+                      <p className="value">{work.role.join(', ')}</p>
+                    </div>
+                    <div className="info">
+                      <p>status</p>
+                      <p className="value">{statusType[work.status]}</p>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section className="section-content works">
+            <h3>Platform Area</h3>
+            <ul>
+              {data.platform.map((work) => (
+                <li>
+                  <div className="work">
+                    <p className="title">{work.title}</p>
+                    <div className="info">
+                      <p>scope:</p>
+                      <p className="value">{work.scope.join(', ')}</p>
+                    </div>
+                    <div className="info">
+                      <p>assigne:</p>
+                      <p className="value">{work.assigne}</p>
+                    </div>
+                    <div className="info">
+                      <p>pm:</p>
+                      <p className="value">{work.pm || 'none'}</p>
+                    </div>
+                    <div className="info">
+                      <p>release plan:</p>
+                      <p className="value">{work.releasePlan || 'TBA'}</p>
+                    </div>
+                    <div className="info">
+                      <p>role(s)</p>
+                      <p className="value">{work.role.join(', ')}</p>
+                    </div>
+                    <div className="info">
+                      <p>status</p>
+                      <p className="value">{statusType[work.status]}</p>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section className="section-content works">
+            <h3>Other Area</h3>
+            <ul>
+              {data.others.map((work) => (
+                <li>
+                  <div className="work">
+                    <p className="title">{work.title}</p>
+                    <div className="info">
+                      <p>assigne:</p>
+                      <p className="value">{work.assigne}</p>
+                    </div>
+                    <div className="info">
+                      <p>role(s)</p>
+                      <p className="value">{work.role.join(', ')}</p>
+                    </div>
+                    <div className="info">
+                      <p>status</p>
+                      <p className="value">{statusType[work.status]}</p>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section className="section-content note">
+            <h3>Note / Concern</h3>
+            <p className={raleway.className}>
+              Seems SPM request due to technical issue is handed over to ID now
+            </p>
+          </section>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Report;
---
+++ app/report/data/4.tsx
@@ -0,0 +1,87 @@
+
+export enum statusType {
+  InProgress,
+  Done,
+  Issue
+}
+
+export const data = {
+  main: [
+    {
+      number: 220894,
+      numberEpic: 220891,
+      title: '[ID-FE][AS] Create permission Doc for email configuration using switch',
+      pm: 'tianpeng.li',
+      assigne: 'yohannes.alldy',
+      scope: ['AS'],
+      role: ['developer'],
+      status: 1,
+      releasePlan: '08.v1'
+    },
+    {
+      number: 221817,
+      numberEpic: 213007,
+      title: '[CS][FE-ID][Admin] Adjust CS Clearing',
+      pm: 'marcelline.michell',
+      assigne: 'condro.wiyono',
+      scope: ['CS'],
+      role: ['reviewer'],
+      status: 1,
+      releasePlan: 'TBA'
+    },
+    {
+      number: 140000,
+      numberEpic: 139985,
+      title: '[AS Project] [FE] Refactor Order Info List',
+      pm: '-',
+      assigne: 'yohannes.alldy',
+      scope: ['AS'],
+      role: ['developer'],
+      status: 0,
+      releasePlan: '08.v1'
+    },
+    {
+      number: 218382,
+      numberEpic: 195376,
+      title: '[Data][FE] Check Request Logic of Real Time Monitoring Request Config',
+      pm: 'ruojie.zeng',
+      assigne: 'yohannes.alldy',
+      scope: ['Real Time Monitor'],
+      role: ['developer'],
+      status: 1,
+      releasePlan: '08.v1 or 08.v2'
+    },
+    {
+      number: 218383,
+      numberEpic: 195376,
+      title: '[Data][FE] Add Clearing Service Monitoring Menu',
+      pm: 'ruojie.zeng',
+      assigne: 'yohannes.alldy',
+      scope: ['Real Time Monitor'],
+      role: ['developer'],
+      status: 1,
+      releasePlan: '08.v1 or 08.v2'
+    },
+  ],
+  platform: [
+    {
+      number: '',
+      numberEpic: '',
+      title: 'Check template page issue for SPM',
+      pm: 'mingkang.lin',
+      assigne: 'yohannes.alldy',
+      scope: ['Platform'],
+      role: ['developer'],
+      status: 1,
+      releasePlan: '-'
+    },
+  ],
+  others: [
+    {
+      title: 'Create SPM V2 request guide',
+      assigne: 'yohannes.alldy',
+      role: ['developer'],
+      status: 1,
+    },
+  ]
+}
